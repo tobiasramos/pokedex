4e1dbaed44e379ccce90480d2cc4fccb71e91ce1
Tobias <tobiasramossilva@gmail.com>
2023-09-12 15:43:41 -0300

    completed the application but with some changes to make in the future

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Pokedex
+A Pokedex é uma incrível ferramenta que reúne informações detalhadas sobre 80 Pokémon diferentes. Com esta aplicação interativa e dinâmica, você pode explorar as características de cada Pokémon, aprender sobre suas habilidades e até mesmo buscar por seus favoritos. A Pokedex possui um design responsivo, que se adapta perfeitamente a qualquer dispositivo, e campos validados para garantir uma experiência de usuário sem problemas.
 
-Projeto em construção.
+## Instalação
+1. Clone o repositório: git clone https://github.com/tobiasramos/pokedex.git
+2. Acesse o diretório do projeto: cd pokedex
+3. Instale as dependências: npm install
+
+## Uso
+1. Execute o projeto localmente: npm start
+2. Abra o navegador e acesse: http://localhost:3000
+
+## Tecnologias utilizadas
+ - HTML
+ - CSS
+ - JAVASCRIPT
+ - REACTJS
+
+## Projeto em execução
+![tela](./tela.gif)
--- src/components/pokemonSearch/pokemonSearch.js
+++ src/components/pokemonSearch/pokemonSearch.js
@@ -6,18 +6,35 @@ import { Link } from "react-router-dom";
 const PokemonSearch = () => {
   const [search, setSearch] = useState("");
   const [pokemon, setPokemon] = useState(null);
+  const [error, setError] = useState(null);
 
   const onChangeHandle = (e) => {
     setSearch(e.target.value);
   };
 
-  const handleSearch = () => {
-    onSearchHandle(search);
+  const handleSearch = async () => {
+    if (search.trim() === "") {
+      setError("O campo de pesquisa está em branco.");
+    } else {
+      onSearchHandle(search);
+    }
   };
 
   const onSearchHandle = async (pokemon) => {
-    const result = await searchPokemon(pokemon);
-    setPokemon(result);
+    try {
+      const result = await searchPokemon(pokemon);
+      setPokemon(result);
+      setError(null); 
+    } catch (err) {
+      setError("Pokémon não encontrado.");
+      setPokemon(null); 
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
   };
 
   return (
@@ -26,8 +43,10 @@ const PokemonSearch = () => {
         placeholder="Digite o nome do Pokémon"
         value={search}
         onChange={onChangeHandle}
+        onKeyPress={handleKeyPress}
       />
       <button onClick={handleSearch}>Buscar</button>
+      {error ? <ErrorMessage>{error}</ErrorMessage> : null}
       {pokemon ? (
         <div>
           <Link to={`/pokemon/${pokemon.id}`}>
@@ -55,6 +74,11 @@ const SearchContainer = styled.div`
     color: #fff;
     background-color: #4e5c69;
   }
+`;
+
+const ErrorMessage = styled.div`
+  color: red;
+  margin-top: 10px;
 `;
 
 export default PokemonSearch;
